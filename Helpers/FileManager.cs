0d812d75b64c3f82ef3828dc6136bb453ed7768b
Kirill Zhestkov <infinity.crime@outlook.com>
2024-12-11 23:23:37 +0300

    Реализовал рекорд кликов за всю историю игры: создан класс, который работает с файлом .txt; сама папка с файлом; изменен файл конфигурации и произведена привязка данных к View.

---
+++ Helpers/FileManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Clicker.Helpers
+{
+    public class FileManager
+    {
+        private readonly string _filePath;
+
+        public FileManager(string filePath)
+        {
+            _filePath = filePath ?? throw new ArgumentNullException(nameof(filePath));
+        }
+
+        public void WriteRecord(int record)
+        {
+            try
+            {
+                using (var writer = new StreamWriter(_filePath, append: false))
+                {
+                    writer.Write(record.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new IOException("Ошибка записи в файл!", ex);
+            }
+        }
+
+        public int ReadRecord()
+        {
+            try
+            {
+                using (var reader = new StreamReader(_filePath))
+                {
+                    var content = reader.ReadLine();
+                    return int.TryParse(content, out int record) ? record : 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new IOException("Ошибка записи в файл!", ex);
+            }
+        }
+    }
+}
